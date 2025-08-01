ca712d73d47392947583f2c13205ec8bc3191091
nadupoy <gnsampao@gmail.com>
2025-08-01 12:45:00 +0300

    HTML elements layout. First commit.

--- src/main.js
+++ src/main.js
@@ -1,24 +1,56 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// import './style.css'
+import errorIcon from "/images/icon-error.svg";
+// import { setupCounter } from "./counter.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+document.querySelector("#app").innerHTML = `
+  <main>
+  <section>
+  <h1>Learn to code by watching others</h1>
+  <p>
+  See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.
+  </p>
+  </section>
+
+  <section>
+  <p>
+  <b>Try it free 7 days</b> then $20/mo. thereafter
+  </p>
+
+  <form>
+  <div class="input-field">
+  <input type="text" placeholder="First Name">
+  <img src="${errorIcon}" alt="error icon" class="error-icon" />
   </div>
-`
+  <p class="error-message"><em>First Name cannot be empty</em></p>
+  <div class="input-field">
+  <input type="text" placeholder="Last Name">
+  <img src="${errorIcon}" alt="error icon" class="error-icon" />
+  </div>
+  <p class="error-message"><em>Last Name cannot be empty</em></p>
+  <div class="input-field">
+  <input type="email" placeholder="Email Address">
+  <img src="${errorIcon}" alt="error icon" class="error-icon" />
+  </div>
+  <p class="error-message"><em>Looks like this is not an email</em></p>
+  <div class="input-field">
+  <input type="password" placeholder="Password">
+  <img src="${errorIcon}" alt="error icon" class="error-icon" />
+  </div>
+  <p class="error-message"><em>Password cannot be empty</em></p>
+  <input type="submit" value="CLAIM YOUR FREE TRIAL">
+  </form>
 
-setupCounter(document.querySelector('#counter'))
+  <p>
+  By clicking the button you are agreeing to our <b>Terms and Services</b>
+  </p>
+  </section>
+  </main>
+
+  <footer>
+  <p>
+  Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a> | Coded by <a href="https://github.com/nadupoy" target="_blank">Nadupoy</a>
+  </p>
+  </footer>
+`;
+
+// setupCounter(document.querySelector("#counter"));
