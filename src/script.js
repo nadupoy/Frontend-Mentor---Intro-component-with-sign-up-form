e7e6f7a8a35f161d7fa5cefc56f76d31fc6880ee
nadupoy <gnsampao@gmail.com>
2025-08-04 16:24:53 +0300

    Defined and exported functions.

--- src/script.js
+++ src/script.js
@@ -1,9 +1,21 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
-}
+// export function setupCounter(element) {
+//   let counter = 0
+//   const setCounter = (count) => {
+//     counter = count
+//     element.innerHTML = `count is ${counter}`
+//   }
+//   element.addEventListener('click', () => setCounter(counter + 1))
+//   setCounter(0)
+// }
+
+/*
+Recieve an error message when the form is submitted if:
+- Any input field is empty.
+- The email address is not formatted correctly.
+*/
+
+export function submitForm() {}
+
+export function checkEmailFormat() {}
+
+export function emptyInputField() {}
